b4bd0e13a31818a384e124c39ef58d0aa90a39e0
namrashaikh15 <146547406+namrashaikh15@users.noreply.github.com>
2023-10-01 00:21:13 +0530

    Update ConvertBST_to_BalancedBST.java

--- BinarySearchTree/ConvertBST_to_BalancedBST.java
+++ BinarySearchTree/ConvertBST_to_BalancedBST.java
@@ -130,3 +130,27 @@ public class ConvertBST_to_BalancedBST {
  * 3 6 10 12
  * 
  */
+*/Node Class: This class defines the structure of a binary tree node. Each node has an integer data value, a left child, and a right child.
+
+levelOrder Function: This function is used to print the BST in level-order (also known as breadth-first traversal). It uses a queue to traverse the tree level by level and prints the nodes at each level, separating them with line breaks.
+
+buildBST Function: This function is used to build a BST. It takes an existing root node and an integer value as input and recursively inserts the value into the BST based on the BST property (left subtree contains values less than the node, right subtree contains values greater than the node).
+
+sortedArray Function: This function performs an in-order traversal of the BST and populates an ArrayList with the node values in sorted order.
+
+balancedBST Function: This function takes a sorted ArrayList and constructs a balanced BST from it. It calculates the middle index of the array and uses the value at that index as the root of the current subtree. It then recursively constructs the left and right subtrees from the portions of the array to the left and right of the middle index.
+
+convertBST_to_balancedBST Function: This function serves as the main driver for the conversion process. It first creates an ArrayList by performing an in-order traversal of the original BST. Then, it calls the balancedBST function to construct a balanced BST using the sorted values.
+
+inOrder Function: This function performs an in-order traversal of a tree and prints its nodes in sorted order.
+
+main Function: The main method initializes an array of integers and constructs an initial BST by inserting the array elements into it. It then prints the original BST in level-order, converts it to a balanced BST using the convertBST_to_balancedBST function, and finally prints the balanced BST in level-order.
+
+Output:
+The program demonstrates the conversion of an unbalanced BST into a balanced BST. It first prints the original unbalanced BST in level-order and then prints the balanced BST in level-order, showing the difference in the structure of the trees.
+
+
+
+
+
+Regenerate
